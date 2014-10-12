7f8bf9ce86d9ce3664892725563a9935ea332a37
Sam Skjonsberg <skoneberg@gmail.com>
2014-10-12 12:45:50 -0700

    Modify cb to use checksums.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var replace = require('gulp-replace');
 var path = require('path');
 var fs = require('fs');
+var checksum = require('checksum');
+var request = require('request');
 
 // Match {{cache-break:path/to/resource}}
 var reCacheBreak = /{{cache-break:(.+?)}}/g;
@@ -13,23 +15,24 @@ var reLeadingSlash = /^\/+/;
 
 module.exports = function(base) {
   base = base && path.resolve(base);
+
   if(!base || !fs.existsSync(base) || !fs.statSync(base).isDirectory()) {
     base = process.cwd();
   }
+
   return replace(reCacheBreak, function(match, resource) {
     var qs;
     var time = Date.now();
 
-    // If we're not dealing with an external file and the file exists, use the
-    // files last modified time as the cache breaker
+    // If we're not dealing with an external file and the file exists, let's
+    // get ourselves a checksum of the file's contents
     if(!reExternal.test(resource)) {
-
       // Since we know it's not external, we interpret the path as relative from
       // the specified base.  This means that in order to determine the full path
       // correclty we'll need to remove a single leading slash.
       var fullPath = path.resolve(base, resource.replace(reLeadingSlash, ''));
       if(fs.existsSync(fullPath)) {
-        qs = fs.statSync(fullPath).mtime.getTime();
+        qs = checksum(fs.readFileSync(fullPath).toString());
       }
     }
 
--- test.js
+++ test.js
@@ -2,47 +2,33 @@ var cb = require('./');
 var assert = require('assert');
 var gutil = require('gulp-util');
 var format = require('util').format;
+var checksum = require('checksum');
 var fs = require('fs');
 
 it('should autoprefix CSS', function(done) {
 	var s = cb('fixture/');
   var path = __dirname + '/fixture/fixture.html';
   var contents = fs.readFileSync(path);
-  var mod = fs.statSync(__dirname + '/fixture/fixture.js').mtime.getTime();
-  var time;
+  var cs = checksum(fs.readFileSync(__dirname + '/fixture/fixture.js').toString());
 
 	s.on('data', function(file) {
-		assert(
-      file.contents.toString().indexOf(
-        format('<script src="fixture.js?cb=%s"></script>', mod)
-      ) !== -1
-    );
-    assert(
-      file.contents.toString().indexOf(
-        format('<script src="fixture.js?cb=%s"></script>', mod)
-      ) !== -1
-    );
-    assert(
-      file.contents.toString().indexOf(
-        format('<script src="/does-not-exist.js?cb=%s"></script>', time)
-      ) !== -1
-    );
-    assert(
-      file.contents.toString().indexOf(
-        format('<script src="//external.js?cb=%s"></script>', time)
-      ) !== -1
-    );
-    assert(
-      file.contents.toString().indexOf(
-        '<script src="/do/not/cache-break.js"></script>'
-      ) !== -1
-    );
+		var fc = file.contents.toString();
+
+		assert(fc.indexOf(format('<script src="fixture.js?cb=%s"></script>', cs)) !== -1);
+
+    assert(fc.indexOf(format('<script src="fixture.js?cb=%s"></script>', cs)) !== -1);
+
+    assert(fc.match(/does-not-exist\.js\?cb=\d{13}/));
+
+		assert(fc.match(/\/\/external\.js\?cb=\d{13}/));
+
+    assert(fc.indexOf('<script src="/do/not/cache-break.js"></script>') !== -1);
+
 		assert.equal(file.relative, 'fixture.html');
 	});
 
 	s.on('end', done);
 
-  time = Date.now();
 	s.write(new gutil.File({
 		cwd: __dirname,
 		base: __dirname + '/fixture',
